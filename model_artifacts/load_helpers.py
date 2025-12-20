a06b7f51e29b02b53cf67817f7b8a03e89f997ff
sznajdr <46231329+sznajdr@users.noreply.github.com>
2025-12-20 09:34:43 +0100

    Update load_helpers.py

--- model_artifacts/load_helpers.py
+++ model_artifacts/load_helpers.py
@@ -9,7 +9,7 @@ class CalibratedModel:
         self.calibrator = calibrator
         self.shrink_strength = shrink_strength
         self.base_rate = base_rate
-    
+
     def predict_proba(self, X):
         raw = self.model.predict_proba(X)[:, 1]
         calibrated = self.calibrator.predict(raw)
@@ -21,13 +21,13 @@ class CalibratedModel:
 def load_models(save_dir='model_artifacts'):
     goal_bundle = joblib.load(f'{save_dir}/goal_model_bundle.pkl')
     assist_bundle = joblib.load(f'{save_dir}/assist_model_bundle.pkl')
-    
+
     goal_model = CalibratedModel(goal_bundle['model'], goal_bundle['calibrator'])
     assist_model = CalibratedModel(
-        assist_bundle['model'], 
+        assist_bundle['model'],
         assist_bundle['calibrator'],
         shrink_strength=assist_bundle.get('shrink_strength', 0),
         base_rate=assist_bundle.get('base_rate', 0)
     )
-    
+
     return goal_model, assist_model, goal_bundle['features'], assist_bundle['features']
